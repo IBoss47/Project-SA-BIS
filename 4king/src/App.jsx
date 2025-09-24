13f9b76e69382bf8ced8af1b256719d44ecdaba3
Thanaphat <sukchuen_t@su.ac.th>
2025-09-24 03:05:16 +0000

    confix

--- 4king/src/App.jsx
+++ 4king/src/App.jsx
@@ -1,14 +1,15 @@
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar.jsx';
 import React from 'react';
-import SaleList from './components/SaleList';
-import FilterSidebar from './components/FilterSidebar';
-
 function App() {
   return (
-    <div className="container">
-      <FilterSidebar />
-      <SaleList />
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar/>
+        <h1>hello</h1>
+      </div>
+    </Router>
   );
 }
 
