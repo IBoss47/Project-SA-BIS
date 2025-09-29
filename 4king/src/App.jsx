c9da7f367effd28a2e5bc7ed743d402619d5c2c0
Thanin Tangkoblap <mail.879tt@gmail.com>
2025-09-29 22:15:18 +0700

    Commit : Modified SalePage.

--- 4king/src/App.jsx
+++ 4king/src/App.jsx
@@ -6,13 +6,14 @@ import Footer from './components/Footer';
 import FilterSidebar from './components/FilterSidebar';
 import Homepage from './pages/Homepage';
 import SellItem from './pages/SellItem';
+import SalePage from './pages/SalePage';
 import React from 'react';
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-            <Route path="/" element={<Homepage />} />
+            <Route path="/" element={<SalePage />} />
             <Route path="/sellitem" element={<SellItem />} />
         </Routes>
       </div>
